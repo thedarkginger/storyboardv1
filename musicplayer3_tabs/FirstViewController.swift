4f6b67c504b98d6deed782c40aba09302baa4a44
thedarkginger <jp.gooderham@gmail.com>
2017-12-07 22:45:27 -0800

    Initial Commit

---
+++ musicplayer3_tabs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  musicplayer3_tabs
+//
+//  Created by JP on 12/7/17.
+//  Copyright © 2017 storyboard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
