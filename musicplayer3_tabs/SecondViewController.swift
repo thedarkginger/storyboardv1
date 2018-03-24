d98996433d1b867ccbf2f76ab1d75762c0f8fa7a
thedarkginger <jp.gooderham@gmail.com>
2018-03-24 12:04:06 -0700

    secondVC update with table -- failing

--- musicplayer3_tabs/SecondViewController.swift
+++ musicplayer3_tabs/SecondViewController.swift
@@ -12,6 +12,8 @@ var audioPlayer:AVAudioPlayer!
 
 class SecondViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
+    var TableData:Array< String > = Array < String >()
+    
     var avPlayer:AVPlayer?
     var avPlayerItem:AVPlayerItem?
     
@@ -22,16 +24,25 @@ class SecondViewController: UIViewController,UITableViewDelegate, UITableViewDat
         subscribeTable.delegate = self
         
         subscribeTable.dataSource = self
+        
+        let defaults = UserDefaults.standard
+        var myarray = defaults.stringArray(forKey: "SavedStringArray") ?? [Array]()
+        
+        TableData.append(myarray)
+
     
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
         
-        let defaults = UserDefaults.standard
-        let myarray = defaults.stringArray(forKey: "SavedStringArray") ?? [String]()
-        
-        return myarray.count
+        return TableData.count
 
+    }
+    
+   func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -43,8 +54,73 @@ class SecondViewController: UIViewController,UITableViewDelegate, UITableViewDat
         
         cell.textLabel?.text = myarray[indexPath.row]
         
+        if myarray.contains(TableData[indexPath.row]){
+            print("something")
+            var imageView : UIImageView
+            imageView  = UIImageView(frame:CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.image = UIImage(named:"checkmark.png")
+            cell.accessoryView = imageView
+            
+        }else{
+            
+            var imageView : UIImageView
+            imageView  = UIImageView(frame:CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.image = UIImage(named:"star.png")
+            imageView.tag = indexPath.row
+            imageView.isUserInteractionEnabled = true
+            let tapgest = UITapGestureRecognizer()
+            tapgest.addTarget(self, action: #selector(tapaccessoryButton(sender:)))
+            
+            imageView.addGestureRecognizer(tapgest)
+            cell.accessoryView = imageView
+            cell.accessoryView?.isUserInteractionEnabled = true
+            
+        }
+        
         return cell
             }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 100.0;//Choose your custom row height
+    }
+    
+    
+    @objc func tapaccessoryButton(sender:UITapGestureRecognizer) {
+        
+        let tag = sender.view?.tag
+        let indexpath = IndexPath(row: tag!, section: 0)
+        if indexpath != nil {
+            
+            self.tableView(tableView, accessoryButtonTappedForRowWith: indexpath)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        // test to see if i can store row name in the defaults array
+        let defaults = UserDefaults.standard
+        var myarray = defaults.stringArray(forKey: "SavedStringArray") ?? [String]()
+        if let datastring = TableData[indexPath.row] as? String {
+            if !myarray.contains(datastring) {
+                myarray.append(datastring)
+                defaults.set(myarray, forKey: "SavedStringArray")
+                defaults.synchronize()
+            }
+        }
+        
+        // this should set the accessory to the second image on click
+        
+        var imageView : UIImageView
+        imageView  = UIImageView(frame:CGRect(x: 0, y: 0, width: 50, height: 50))
+        imageView.image = UIImage(named:"checkmark.png")
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        //cell?.accessoryType = .checkmark
+        cell?.accessoryView = imageView
+        
+        print("it works")
+        
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // cell selected code here
@@ -55,6 +131,7 @@ class SecondViewController: UIViewController,UITableViewDelegate, UITableViewDat
         // Dispose of any resources that can be recreated.
     }
     
+
     @IBOutlet var subscribeTable: UITableView!
     
 }
