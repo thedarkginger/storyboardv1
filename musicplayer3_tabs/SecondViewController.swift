27b5bdd96f57bbbc9c1bb629c233669a9512f0c5
thedarkginger <jp.gooderham@gmail.com>
2018-03-24 01:58:52 -0700

    secondView now shows table of subscribed shows

--- musicplayer3_tabs/SecondViewController.swift
+++ musicplayer3_tabs/SecondViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 import AVFoundation
 var audioPlayer:AVAudioPlayer!
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     var avPlayer:AVPlayer?
     var avPlayerItem:AVPlayerItem?
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        subscribeTable.delegate = self
         
+        subscribeTable.dataSource = self
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        let defaults = UserDefaults.standard
+        let myarray = defaults.stringArray(forKey: "SavedStringArray") ?? [String]()
+        
+        return myarray.count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as UITableViewCell
+        
+        let defaults = UserDefaults.standard
+        let myarray = defaults.stringArray(forKey: "SavedStringArray") ?? [String]()
+        
+        cell.textLabel?.text = myarray[indexPath.row]
+        
+        return cell
+            }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // cell selected code here
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,5 +55,6 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet var subscribeTable: UITableView!
     
 }
