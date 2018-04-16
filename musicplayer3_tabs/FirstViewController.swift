c83f72e4d81308d4292ff4eb3d1ca9dd524c0b68
thedarkginger <jp.gooderham@gmail.com>
2018-04-15 22:13:50 -0700

    added cell height to first vc

--- musicplayer3_tabs/FirstViewController.swift
+++ musicplayer3_tabs/FirstViewController.swift
@@ -76,6 +76,11 @@ class FirstViewController: UIViewController,UITableViewDelegate, UITableViewData
         return 1
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 80.0;//Choose your custom row height
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as UITableViewCell
